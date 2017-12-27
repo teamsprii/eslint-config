6515691a95070bde93e0453a7688895d9d6d6fb6
carsy <jlageb@gmail.com>
2017-12-27 10:18:09 +0400

    Initial commit

---
+++ es5.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    extends: [
+        './rules/best-practices',
+        './rules/errors',
+        './rules/strict',
+        './rules/style',
+        './rules/variables',
+    ].map(require.resolve),
+    env: {},
+    globals: {
+        'Promise': true,
+    },
+    rules: {},
+};
