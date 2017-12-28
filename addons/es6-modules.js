609384b71fd047ea8a6d560f14dadb0613a243c1
carsy <jlageb@gmail.com>
2017-12-28 14:29:32 +0400

    Remove ecmaVersion override in es6-modules addon

--- addons/es6-modules.js
+++ addons/es6-modules.js
@@ -6,12 +6,11 @@
 module.exports = {
     parserOptions: {
         sourceType: 'module',
-        ecmaVersion: 6,
     },
     rules: {
         // rule to control usage of strict mode directives
         // comment: code inside es6 modules is under strict mode by default, so it's not necessary
-        'strict': 0,
+        'strict': [2, 'never'],
         // enforce require() on top-level module scope
         // comment: since we are using es6 modules, one will be using import instead of require
         'global-require': 0,
